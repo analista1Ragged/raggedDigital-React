901d1f2c13e47a2d5ef433a789dbe67d897ab8d8
analista1Ragged <analistasolucionesti1@ragged.com.co>
2024-06-25 10:05:26 -0500

    Se modifica el login

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,47 +1,64 @@
-import { useState } from "react"
-import "./login.css"
-import CampoTexto from "../CampoTexto"
-import Boton from "../Boton/Boton"
-
+import { useState } from "react";
+import axios from "axios";
+import "./login.css";
+import CampoTexto from "../CampoTexto";
+import Boton from "../Boton/Boton";
+import { useNavigate } from "react-router-dom";
+ 
 const Login = (props) => {
-
-    const [usuario, actualizarNombre] = useState("")
-    const [contraseña, actualizarPuesto] = useState("")
-
-    const { registrarColaborador } = props
-
-    const manejarEnvio = (e) => {
-        e.preventDefault()
-        console.log("Manejar el envio")
-        let datosAEnviar = {
-            usuario,
-            contraseña
+    const [usuario, actualizarNombre] = useState("");
+    const [contrasena, actualizarContrasena] = useState("");
+    const navigate = useNavigate();
+ 
+    const manejarEnvio = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://127.0.0.1:5000/logon', {
+                usuario: usuario,
+                contrasena: contrasena
+               
+            });
+            if (response.data.message === "success") {
+                alert("Login exitoso");
+               
+                navigate("/Home"); // Redirige a la página de inicio
+            }
+        } catch (error) {
+            alert("Usuario y/o contraseña incorrectos");
+            //alert("error");
+            console.error("Error en el login:", error);
         }
-        registrarColaborador(datosAEnviar)
-    } 
-
-    return <section className="formulario">
-        
-        <div className="image-container">
-        <img src={require("../../assets/Images/header.jpg")} alt='TiendaRagged' />
-        </div>
-
-        <form >
-            <div className="form-container">
-                <h2>¡ Bienvenido a Ragged Digital !</h2>
-                <CampoTexto
-                    titulo="Contraseña"
-                    placeholder="Ingresar Contraseña:"
-                    required
-                    valor={contraseña}
-                    actualizarValor={actualizarPuesto}
-                />
-                <Boton>
-                    Ingresar
-                </Boton>
-            </div>        
-        </form>
-    </section>
-}
-
-export default Login
+    };
+ 
+    return (
+        <section className="formulario">
+            <div className="image-container">
+                <img src={require("../../assets/Images/header.jpg")} alt='TiendaRagged' />
+            </div>
+            <form onSubmit={manejarEnvio}>
+                <div className="form-container">
+                    <h2>¡Bienvenido a Ragged Digital!</h2>
+                    <CampoTexto
+                        titulo="Usuario"
+                        placeholder="Ingresar Usuario:"
+                        required
+                        valor={usuario}
+                        actualizarValor={actualizarNombre}
+                    />
+                    <CampoTexto
+                        titulo="Contraseña"
+                        placeholder="Ingresar Contraseña:"
+                        required
+                        valor={contrasena}
+                        actualizarValor={actualizarContrasena}
+                    />
+                    <Boton>
+                        Ingresar
+                    </Boton>
+                </div>
+            </form>
+        </section>
+    );
+};
+ 
+export default Login;
