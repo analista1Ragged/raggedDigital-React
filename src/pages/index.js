b71521a37f9a10a16451a5ab1ca9433d55aff433
analista1Ragged <analistasolucionesti1@ragged.com.co>
2025-01-08 10:55:20 -0500

    Se sube modulo Reporte Nomina Dane

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,3 +15,4 @@ export { default as ReporteReferencias } from './ReporteReferencias/ReporteRefer
 export { default as ReportesCostos } from './ReporteCostos/ReportesCostos';
 export { default as EscalasPrecios } from './EscalasPrecios/EscalasPrecios';
 export { default as Layout } from './Layout';
+export { default as ReporteDane } from './ReporteDane/ReporteDane';
