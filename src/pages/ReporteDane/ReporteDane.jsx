82d672d76b007ca940cf776815d1c96ae2916255
AnalistaRagged <analistasolucionesti@ragged.com.co>
2025-01-09 16:17:54 -0500

    reporte nomina dane

--- src/pages/ReporteDane/ReporteDane.jsx
+++ src/pages/ReporteDane/ReporteDane.jsx
@@ -1,15 +1,96 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./ReporteDane.css";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Pagination, Tag } from 'antd';
 import 'antd/dist/reset.css';
 import BotonBuscar from 'src/components/BotonBuscar/BotonBuscar.jsx';
 import { Select } from 'antd'; // Importa el componente Select de Ant Design
+import { urlapi } from '../../App';
+import Swal from 'sweetalert2';
+
 const { Option } = Select;
-
-
-
 const ReporteDane = () => {
+  const [tablaDatos, setTablaDatos] = useState([]);
+  const [periodos, setPeriodos] = useState([]);
+
+  // Función para obtener los periodos del endpoint
+  const fetchPeriodos = async () => {
+    try {
+      const response = await fetch(`${urlapi}/nomina/get-RepDane`);
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      
+      // Leer el cuerpo de la respuesta como texto
+      const textData = await response.text();
+      
+      // Reemplazar 'NaN' con 'null' para que sea JSON válido
+      const cleanedData = textData.replace(/NaN/g, "null");
+      
+      // Convertir el texto limpio a JSON
+      const data = JSON.parse(cleanedData);
+      
+      // Filtrar los periodos válidos (numéricos)
+      const filteredPeriodos = data
+        .flatMap(row => row.filter(item => item !== null && !isNaN(item))) // Filtrar los valores válidos
+        .map(item => String(item)); // Convertir a string si es necesario
+      
+      setPeriodos(filteredPeriodos);
+    } catch (error) {
+      console.error('Error al obtener los periodos:', error);
+    }
+  };
+  
+
+  // Llamar fetchPeriodos al cargar el componente
+  useEffect(() => {
+    fetchPeriodos();
+  }, []);
+
+  const handleBuscarPeriodo = async () => {
+    const selectedPeriodo = document.querySelector(".ant-select-selection-item")?.innerText;
+  
+    if (!selectedPeriodo) {
+      alert("Por favor selecciona un periodo antes de buscar.");
+      return;
+    }
+  
+    try {
+      Swal.fire({
+        title: "Cargando Datos",
+        text: "Por favor espera...",
+        allowOutsideClick: false,
+        didOpen: () => {
+          Swal.showLoading();
+        },
+      });
+  
+      const response = await fetch(`${urlapi}/nomina/post-RepDane`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ periodo: selectedPeriodo }),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+  
+      const result = await response.json();
+      setTablaDatos(result);
+  
+      Swal.close();
+    } catch (error) {
+      console.error("Error al obtener los datos del periodo:", error);
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "No se pudieron cargar los datos.",
+      });
+    }
+  };
+
   return (
     <section>
       <div className="ticket-table">
@@ -24,20 +105,24 @@ const ReporteDane = () => {
         <div className="col-12 col-md-5">
           <label htmlFor="marca" className="label-spacing">Periodo</label>
           <div>
-            <Select
-              style={{ width: 350 }}
-              showSearch
-              placeholder="Selecciona un Periodo"
-              className="w-100"
-            >
-              {/* Opciones de ejemplo */}
-              <Option value="capsula1">202412</Option>
-              <Option value="capsula1">202411</Option>
-            </Select>
+                <Select
+                  style={{ width: 350 }}
+                  showSearch
+                  placeholder="Selecciona un Periodo"
+                  className="w-100"
+                >
+                  {periodos.map((periodo, index) => (
+                    <Option key={index} value={periodo}>
+                      {periodo}
+                    </Option>
+                  ))}
+                </Select>
           </div>
         </div>
         <div className="col-12 col-md-5">
-            <BotonBuscar/>
+          <BotonBuscar onClick={handleBuscarPeriodo} />
+
+            
         </div>
       </div>
     </form> 
@@ -94,46 +179,49 @@ const ReporteDane = () => {
               <th scope="col">Vacaciones</th>
             </tr>
           </thead>
-            <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>1000348131</td>
-                  <td>RIVERA BEJARANO MICHAEL ANDRES</td>
-                  <td>ANALISTA AUDITORIA INTERNA</td>
-                  <td>M</td>
-                  <td>CENTRO DE DISTRIBUCION</td>
-                  <td>OFICINA PRINCIPAL</td>
-                  <td>MEDELLIN</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>X</td>
-                  <td>2691100</td>
-                  <td>29</td>
-                  <td>2578971</td>
-                  <td>0</td>
-                  <td>0</td>
-                  <td>0</td>
-                  <td>0</td>
-                  <td>0</td>
-                  <td>0</td>
-                  <td>4036650</td>
-                  <td>107644</td>
-                  <td>107644</td>
-                  <td>0</td>
-                  <td>0</td>
-                  <td>0</td>
-                  <td>228744</td>
-                  <td>322932</td>
-                  <td>21071</td>
-                  <td>53822</td>
-                  <td>80733</td>
-                  <td>107644</td>
-                  <td>336253</td>
-                  <td>336253</td>
-                  <td>40367</td>
-                  <td>107543</td>
-                </tr>
-            </tbody>
+          <tbody>
+            {tablaDatos.map((row, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{row.DOCUMENTO}</td>
+                <td>{row["NOMBRE EMPLEADO"]}</td>
+                <td>{row.CARGO}</td>
+                <td>{row.CCOSTOS}</td>
+                <td>{row.SEXO}</td>
+                <td>{row.CO}</td>
+                <td>{row.UBICACION}</td>
+                <td>{row.OPERARIO || "-"}</td>
+                <td>{row["TECNICO-TECNOLOGO"] || "-"}</td>
+                <td>{row.PROFESIONAL || "-"}</td>
+                <td>{row["SALARIO BASICO"]}</td>
+                <td>{row["DIAS LIQUIDADOS"]}</td>
+                <td>{row["SALARIO DEVENGADO"]}</td>
+                <td>{row["HORAS EXTRAS"]}</td>
+                <td>{row["RECARGOS NOCTURNOS"]}</td>
+                <td>{row["RECARGOS NOCTURNOS"]}</td>
+                <td>{row["TRABAJO DOMINICAL-FESTIVO"]}</td>
+                <td>{row.INCAPACIDADES}</td>
+                <td>{row["AUXILIO TRANSPORTE"]}</td>
+                <td>{row["TOTAL DEVENGADO"]}</td>
+                <td>{row.SALUD1}</td>
+                <td>{row.PENSION1}</td>
+                <td>{row["FONDO SOLIDARIDAD PENS"]}</td>
+                <td>{row["RETENCION EN LA FUENTE"]}</td>
+                <td>{row["OTRAS DEDUCCIONES"]}</td>
+                <td>{row.SALUD2}</td>
+                <td>{row.PENSION2}</td>
+                <td>{row.ARL}</td>
+                <td>{row.SENA}</td>
+                <td>{row.ICBF}</td>
+                <td>{row["CAJA COMPENSACION"]}</td>
+                <td>{row["PRIMA DE SERVICIOS"]}</td>
+                <td>{row.CESANTIAS}</td>
+                <td>{row["INTERESES A LAS CESANTIAS"]}</td>
+                <td>{row.VACACIONES}</td>
+              </tr>
+            ))}
+          </tbody>
+
             </table>
             <div className="container-2">
           </div>
